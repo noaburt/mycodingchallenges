c38d6911732183151dbc7854da39b7c1167a437a
Noa Burt <noaburt051221@gmail.com>
2024-08-22 21:58:32 +0100

    Created blank project

---
+++ wcTool/main.h
@@ -0,0 +1,12 @@
+
+
+/*
+
+Coding Challenges | John Crickett
+
+Name: Noa Burt
+Date: 22/08/2024
+
+This is the header file for this coding challenge
+
+*/
---
+++ wcTool/test.c
@@ -0,0 +1,18 @@
+
+
+/*
+
+Coding Challenges | John Crickett
+
+Name: Noa Burt
+Date: 22/08/2024
+
+This is the file to run all tests required of this coding challenge
+
+*/
+
+int main() {
+    return 0;
+}
+
+
