6a5ea00b828bdb205f5e43a5ff19058aaa3b8bc0
Noa Burt <noaburt051221@gmail.com>
2024-09-06 22:15:24 +0100

    Parsing url into sections for GET request

--- ownCurl/functions.c
+++ ownCurl/functions.c
@@ -13,3 +13,56 @@ This is the functions file for this coding challenge
 #include <main.h>
 
 
+parsedurl* parseURL(char* fullurl) {
+
+  /* parse inputted url and return struct containing parsed data */
+
+  char* urlptr = fullurl;
+
+  if ( !fullurl ) {
+    errx(1, "url pointer is NULL");
+  }
+
+  parsedurl* parsed = malloc( sizeof(*parsedurl) );
+
+  while ( *urlptr != ':' ) {
+    /* find end of protocol */
+
+    urlptr++;
+  }
+
+  switch( *(urlptr - 1) ) {
+  case 's': parsed->protocol = HTTPS;
+  case 'p': parsed->protocol = HTTP;
+  default: errx(1, "no protocol identified");
+  }
+
+  /* skip :// and get host name and port */
+  
+  urlptr += 2;
+  while ( *urlptr != '/' && *urlptr != ':' ) {
+    if ( *urlptr == ':' ) {
+      // GET PORT
+    }
+
+    // OTHERWISE GET HOST
+    
+    urlptr++;
+  }
+  
+  return parsed;
+}
+
+void* freeurl(parsedurl* parsed) {
+  /* free pointers stored by parsed url structure */
+
+  if ( !parsed ) {
+    return;
+  }
+
+  free(parsed->protocol);
+  free(parsed->host);
+  free(parsed->path);
+  free(parsed->port);
+
+}
--- ownCurl/main.h
+++ ownCurl/main.h
@@ -12,9 +12,28 @@ This is the header file for this coding challenge
 
 /* includes */
 
+#include <stdio.h>
+#include <err.h>
+
+
+/* structs */
+
+typedef struct PARSEDURL {
+
+  /* struct to store parts of url for requests */
+
+  enum PROTOCOL {HTTP, HTTPS} protocol;
+  
+  char* host;
+  char* path;
+
+  long port;
+  
+} parsedurl;
 
 
 /* functions */
 
-
+parsedurl* parseURL(char* fullurl);
+void* freeurl(parsedurl* parsed);
 
